a099b5b47843fe0ad206738d87a560b4b6aa8386
zangshuhui <zangshuhui_yanda@163.com>
2019-12-05 15:40:35 +0800

    整个项目第一次提交

---
+++ src/main/java/com/boot/ctroller/HelloController.java
@@ -0,0 +1,19 @@
+package com.boot.ctroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zangshuhui
+ * @create 2019/12/5 - 14:54
+ * @Version 1.0
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("info")
+    public String info(){
+        return "Hello SpringBoot";
+
+    }
+}
